38d229b4b676ae142fee1817c98cb9c47db7ac06
Stepan Skichko <st.skichko@gmail.com>
2023-01-27 09:57:51 +0200

    task 9 DONE
    task 8 IN PROGRESS

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnRef = document.querySelector('.change-color');
+const colorVisualAidRef = document.querySelector('.color');
+const tagBodyRef = document.querySelector('body');
+
+changeColorBtnRef.addEventListener('click', event => {
+  colorVisualAidRef.textContent = getRandomHexColor();
+  tagBodyRef.style.backgroundColor = getRandomHexColor();
+});
