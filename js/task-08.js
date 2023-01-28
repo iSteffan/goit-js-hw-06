9feda70a226e2008eb685e2a7e797d4bec641904
Stepan Skichko <st.skichko@gmail.com>
2023-01-28 10:25:13 +0200

    task 8 DONE

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+
+  const objData = {
+    email,
+    password,
+  };
+
+  if (email === '' || password === '') {
+    return alert('Write down all data in the fields');
+  }
+
+  console.log(objData);
+
+  event.currentTarget.reset();
+});
