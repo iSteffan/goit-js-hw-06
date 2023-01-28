f4f62c232fa07da218deeebec1b98b8dbf930623
Stepan Skichko <st.skichko@gmail.com>
2023-01-28 13:42:01 +0200

    task 10 IN PROGRESS

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputRef = document.querySelector('input[type="number"]');
+const createBtnRef = document.querySelector('button[data-create]');
+const destroyBtnRef = document.querySelector('button[data-destroy]');
+const collectionParentRef = document.querySelector('#boxes');
+
+let boxesAmount = 0;
+
+const inputHandler = event => {
+  boxesAmount = Number(event.currentTarget.value);
+  console.log(boxesAmount);
+};
+
+function createBoxes(amount) {
+  const collectionEl = document.createElement('div');
+
+  for (let i = 0; i <= amount; i += 1) {
+    collectionEl.style.width = `${i * 10 + 30}px`;
+    collectionEl.style.height = `${i * 10 + 30}px`;
+  }
+}
+
+function destroyBoxes() {}
+
+inputRef.addEventListener('input', inputHandler);
+createBtnRef.addEventListener('click', createBoxes);
+destroyBtnRef.addEventListener('click', destroyBoxes);
+
+console.log(inputRef);
+console.log(createBtnRef);
+console.log(destroyBtnRef);
