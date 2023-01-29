09ed3dc4e5e8710fb20de9616a211c69aaa7eaf1
Stepan Skichko <st.skichko@gmail.com>
2023-01-29 15:11:21 +0200

    task 10 DONE

--- js/task-10.js
+++ js/task-10.js
@@ -5,30 +5,39 @@ function getRandomHexColor() {
 const inputRef = document.querySelector('input[type="number"]');
 const createBtnRef = document.querySelector('button[data-create]');
 const destroyBtnRef = document.querySelector('button[data-destroy]');
-const collectionParentRef = document.querySelector('#boxes');
+const collectionOfBoxesRef = document.querySelector('#boxes');
 
 let boxesAmount = 0;
 
 const inputHandler = event => {
   boxesAmount = Number(event.currentTarget.value);
-  console.log(boxesAmount);
 };
 
 function createBoxes(amount) {
-  const collectionEl = document.createElement('div');
+  let boxArray = [];
 
-  for (let i = 0; i <= amount; i += 1) {
+  for (let i = 0; i < amount; i += 1) {
+    const collectionEl = document.createElement('div');
     collectionEl.style.width = `${i * 10 + 30}px`;
     collectionEl.style.height = `${i * 10 + 30}px`;
+    collectionEl.style.backgroundColor = getRandomHexColor();
+
+    boxArray.push(collectionEl);
   }
+
+  return boxArray;
 }
 
-function destroyBoxes() {}
+function destroyBoxes() {
+  collectionOfBoxesRef.innerHTML = '';
+}
 
 inputRef.addEventListener('input', inputHandler);
-createBtnRef.addEventListener('click', createBoxes);
-destroyBtnRef.addEventListener('click', destroyBoxes);
 
-console.log(inputRef);
-console.log(createBtnRef);
-console.log(destroyBtnRef);
+createBtnRef.addEventListener('click', () => {
+  collectionOfBoxesRef.append(...createBoxes(boxesAmount));
+});
+
+destroyBtnRef.addEventListener('click', () => {
+  destroyBoxes();
+});
